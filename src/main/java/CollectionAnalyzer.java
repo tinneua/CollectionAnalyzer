8e5af71a462a180500ff6c1f452a15348114c7fe
tinneua <roma.lugo@gmail.com>
2016-05-10 21:45:14 +0300

    Maven, Ant added

---
+++ src/main/java/CollectionAnalyzer.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.*;
+
+public class CollectionAnalyzer {
+
+    public static void main(String[] args) throws IOException {
+        int size = 10000;
+        String dir = System.getProperty("user.dir");
+        String fileName = String.valueOf(size) + ".txt";
+        Map<Object, Row> temp = new CollectionAnalyzer().launchAnalyzer(size);
+        Printer.printToConsole(temp);
+        Printer.printToFile(temp, fileName, dir);
+    }
+
+    public Map<Object, Row> launchAnalyzer(int size) {
+
+        List<Integer> testArrayList = new ArrayList<>();
+        List<Integer> testLinkedList = new LinkedList<>();
+        Set<Integer> testHashSet = new HashSet<>();
+        Set<Integer> testTreeSet = new TreeSet<>();
+
+        Row arrayListResult = new ListOperations(testArrayList, size, "ArrayList").output();
+        Row linkedListResult = new ListOperations(testLinkedList, size, "LinkedList").output();
+        Row hashSetResult = new SetOperations(testHashSet, size, "HashSet").output();
+        Row treeSetResult = new SetOperations(testTreeSet, size, "TreeSet").output();
+
+        Map<Object, Row> result = new LinkedHashMap<>();
+
+        Row namings = new Row("Type of array", "add", "get", "remove", "contains", "populate", "iterator.add",
+                "iterator.remove");
+        result.put(namings.s1, namings);
+        result.put(arrayListResult.s1, arrayListResult);
+        result.put(linkedListResult.s1, linkedListResult);
+        result.put(hashSetResult.s1, hashSetResult);
+        result.put(treeSetResult.s1, treeSetResult);
+
+        return result;
+    }
+}
---
+++ src/main/java/Printer.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Printer {
+    public static void printToConsole(Map<Object, Row> result) {
+        for (Row row: result.values()) {
+            System.out.print(row.toString());
+        }
+
+    }
+    public static void printToFile(Map<Object, Row> result, String fileName, String dir) throws IOException {
+
+        FileWriter outputFile = new FileWriter(new File(dir, fileName));
+
+        for (Row row: result.values()) {
+            outputFile.write(row.toString());
+        }
+
+        outputFile.close();
+    }
+}
